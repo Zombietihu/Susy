f809bdff04c105b55f7d296d7ef71b965c997c4f
zombietihu <tonatihu.fi@gmail.com>
2015-07-14 00:11:43 -0500

    Susy

--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
-from flask import request, redirect, render_template, url_for
-
+from flask import request, redirect, render_template, url_for, flash, Flask, session, abort
 from app import app
 
+
 @app.route('/')
-@app.route('/welcom')
 def index():
-	return render_template('welcome.html')
+	    return render_template('welcome.html')
+
+@app.route('/home')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+	    return render_template('home.html')
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return index()
