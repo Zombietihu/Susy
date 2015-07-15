229ddbdc750ebf25d2a5821fc4409bda32bac1c3
zombietihu <tonatihu.fi@gmail.com>
2015-07-15 00:17:59 -0500

    Susy

--- app/views.py
+++ app/views.py
@@ -13,10 +13,18 @@ def home():
     else:
 	    return render_template('home.html')
 
+    
 @app.route('/login', methods=['POST'])
 def do_admin_login():
+    
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
         session['logged_in'] = True
     else:
         flash('wrong password!')
-    return index()
+        
+    return home()
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return home()
